1315d8fd769118bd69ff57b03bbcbbf13b097656
Sr.Dayran <dayranrs2015@gmail.com>
2019-05-21 20:06:15 -0300

    parte 2

---
+++ src/produto.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author darai
+ */
+public class produto {
+    private String nome;
+    private int quantidade;
+    private float preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+   
+    
+}
